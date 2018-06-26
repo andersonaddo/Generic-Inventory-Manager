e4f05d63e21ceede146b445ae76554f7149e80d9
Kwadwo A. Yeboah-Asare Jnr <ukabapan@gmail.com>
2018-06-26 23:29:48 +0000

    Revert "Search Inventory"
    This reverts commit 908f2706f9ad865db1078a6e7a45e7bae331dffb.

--- ChemicalInventoryManager/src/chemicalinventorymanager/Screens/SearchScreenController.java
+++ ChemicalInventoryManager/src/chemicalinventorymanager/Screens/SearchScreenController.java
@@ -5,20 +5,9 @@
  */
 package chemicalinventorymanager.Screens;
 
-import chemicalinventorymanager.DatabaseManager;
-//import chemicalinventorymanager.InventoryItem;
-import java.util.List;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
-//import javafx.scene.input.InputMethodEvent;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-
 
 /**
  * FXML Controller class
@@ -26,44 +15,13 @@ import javafx.scene.control.ListView;
  * @author ADDO_a
  */
 public class SearchScreenController implements Initializable {
-    
-    @FXML
-    private TextField SearchTerm;
 
-    @FXML
-    private ComboBox Filter;
-    
-    @FXML
-    private ListView ResultsView;
-    
-    @FXML
-    void search(ActionEvent event) throws SQLException {
-       
-        String search = SearchTerm.getText();
-        String filter = Filter.getValue().toString();
-        if(!search.isEmpty()){
-            List<String> SearchResults;
-            if(filter.equals("All")){
-                SearchResults = DatabaseManager.searchEntireDatabase(search);
-            }else{
-                SearchResults = DatabaseManager.searchWtihFilter(search, filter);
-
-                ResultsView.getItems().clear();
-                
-            }
-            for(String Result:SearchResults){
-                    ResultsView.getItems().add(Result);
-            }
-        }
-        
-    }
-    
-    
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Filter.getItems().clear();
-        Filter.getItems().addAll("All", "Inventory Items", "Customers", "Suppliers", "Transactions");
-        Filter.getSelectionModel().select(0);
+        // TODO
     }    
     
 }
