22c3b632374f6bfe05d92c2a14d8befcc586aa73
Ekow Bentsi-Enchill <ebe4me@msn.com>
2018-06-27 16:07:46 +0000

    added a few things to make the date be returned as a string

--- ChemicalInventoryManager/src/chemicalinventorymanager/Transaction.java
+++ ChemicalInventoryManager/src/chemicalinventorymanager/Transaction.java
@@ -1,5 +1,7 @@
 package chemicalinventorymanager;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +10,7 @@ import java.util.Map;
  *
  */
 public class Transaction {
+    DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
     private String customerID, id;
     Date date;
     transactionMode mode;
@@ -16,6 +19,15 @@ public class Transaction {
     
     public String getID() {return id;}
     public String getCustomerID() {return customerID;}
+    public String getDate() {return df.format(date);}
+    
+    public String getMode() {
+        if (mode == transactionMode.debit) return "debit";
+        return "credit";
+    }
+    
+    public Map<String, Integer> getTransactions() {return transactions;    }
+    public Double getCreditAmount() {return creditAmount;}
     
     public Transaction(String id, String cid){
         this.id = id;
