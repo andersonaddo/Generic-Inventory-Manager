7adda559033659b103a434781d5ae5b6b5c01a57
Anderson Addo <34724755+andersonaddo@users.noreply.github.com>
2018-06-27 18:19:02 -0400

    Added a mssing import

--- ChemicalInventoryManager/src/chemicalinventorymanager/Screens/ItemSummaryController.java
+++ ChemicalInventoryManager/src/chemicalinventorymanager/Screens/ItemSummaryController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
